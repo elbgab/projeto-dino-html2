ef226ea93c78df79e3b13d611d8f5e1cf4be49a9
elbgab <160151714+elbgab@users.noreply.github.com>
2025-10-06 08:28:28 -0300

    commit inicial

---
+++ service_worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", event =>{
+    console.log("Service worker instalado");
+});
+
+self.addEventListener("activate", event => {
+    console.log("Service worker ativado");
+});
+
+self.addEventListener("fetch", event =>{
+    
+});
